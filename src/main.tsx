4cbfb289242be378e3361e39d1e778e369328fd7
Tiago-0liveira <tfgoliveira@gmail.com>
2025-04-06 19:06:34 +0100

    (feat): now using react-scan to profile the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 import { AppSettingsProvider } from '@/components/providers/settings-provider'
 import Langs from "@/lang"
 import { BrowserRouter as Router } from 'react-router-dom'
-import { SocketProvider } from '@/components/providers/socket-provider.tsx'
+import { SocketProvider } from '@/components/providers/socket-provider'
 
+
+if (import.meta.env.DEV) {
+	import("react-scan").then(({useScan}) => {
+		useScan()
+	});
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-	<AppSettingsProvider availableLanguages={Langs}>
-		<Router>
-			<div className="flex size-full flex-col items-center">
-				<SocketProvider>
-					<App />
-				</SocketProvider>
-			</div>
-		</Router>
-	</AppSettingsProvider>
-  </StrictMode>,
+	<StrictMode>
+		<AppSettingsProvider availableLanguages={Langs}>
+			<Router>
+				<div className="flex size-full flex-col items-center">
+					<SocketProvider>
+						<App />
+					</SocketProvider>
+				</div>
+			</Router>
+		</AppSettingsProvider>
+	</StrictMode>,
 )
